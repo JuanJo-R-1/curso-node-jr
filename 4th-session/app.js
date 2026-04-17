55acfe17b976ad83dd1e6639d390b51da20b59c7
JuanJo Rivero <riverocamargojuanjose@gmail.com>
2026-04-17 14:15:01 -0500

    Avanze de estudio 6to día

--- 4th-session/app.js
+++ 4th-session/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const crypto = require('node:crypto')
-const movies = require('./movies.json')
-const cors= required('cors')
-const { validateMovie, validatePartialMovie } = require('./movies')
-const { addAbortListener } = require('node:events')
+import express, { json } from 'express'
+import { randomUUID } from 'node:crypto'
+import cors from 'cors'
+
+import movies from './movies.json'
+import { validateMovie, validatePartialMovie } from './schemas/movies.js'
 
 const app = express()
-app.use(express.json())
+app.use(json())
 app.use(cors({
     origin: (origin, callback) => {
        
@@ -38,7 +38,7 @@ app.get('/movies', (req, res) => {
 
     const { genre } = req.query
     if (genre) {
-        const filteredMovies = movies.filter(
+        const filteredMovies = filter(
             movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
         )
         return res.json(filteredMovies)
@@ -48,20 +48,20 @@ app.get('/movies', (req, res) => {
 
 app.get('/movies/:id', (req, res) => {
     const {id} = req.params
-    const movie = movies.find(movie => movie.id == id)
+    const movie = find(movie => movie.id == id)
     if (movie) return res.json(movie)
     res.status(404).json({ message: 'Movie not found' })
 })
 
 app.delete('/movies/:id', (req, res) => {
   const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
+  const movieIndex = findIndex(movie => movie.id === id)
 
   if (movieIndex === -1) {
     return res.status(404).json({ message: 'Movie not found' })
   }
 
-  movies.splice(movieIndex, 1)
+  splice(movieIndex, 1)
 
   return res.json({ message: 'Movie deleted' })
 })
@@ -75,11 +75,11 @@ app.post('/movies', (req, res) => {
     }
 
     const newMovie = {
-        id: crypto.randomUUID(),
+        id: randomUUID(),
         ...result.data
     }
 
-    movies.push(newMovie)
+    push(newMovie)
     res.status(201).json(newMovie)
 })
 
@@ -91,7 +91,7 @@ app.patch('/movies/:id', (req, res) => {
     }
 
     const {id} = req.params
-    const movieIndex = movies.findIndex(movie => movie.id == id)
+    const movieIndex = findIndex(movie => movie.id == id)
 
     if (movieIndex == -1) {
         return res.status(404).json({message: 'Movie not found'})
@@ -110,4 +110,4 @@ const PORT = process.env.PORT ?? 3004
 
 app.listen(PORT, () => {
     console.log(`server listening on port http://localhost:${PORT}`)
-})
+}) 
--- 4th-session/movies.js
+++ 4th-session/movies.js
@@ -1,19 +1,19 @@
-const z = require('zod')
+import { object, string, number, array, enum } from 'zod'
 
-const movieSchema = z.object({
-    title: z.string({
+const movieSchema = object({
+    title: string({
         invalid_type_error: 'Movie title must be a string',
         required_error: 'Movie title is required.'
     }),
-    year: z.number().int().min(1900).max(2026),
-    director: z.string(),
-    duration: z.number().int().positive(),
-    rate: z.number().min(0).max(10).default(5),
-    poster: z.string().url({
+    year: number().int().min(1900).max(2026),
+    director: string(),
+    duration: number().int().positive(),
+    rate: number().min(0).max(10).default(5),
+    poster: string().url({
         message: 'Poster must be a valid URL'
     }),
-    genre: z.array(
-        z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama',
+    genre: array(
+        enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama',
             'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
         {
             required_error: 'Movie genre is required',
@@ -30,4 +30,4 @@ function validatePartialMovie(input) {
     return movieSchema.partial().safeParse(input)
 }
 
-module.exports = { validateMovie, validatePartialMovie }
+export default { validateMovie, validatePartialMovie }
